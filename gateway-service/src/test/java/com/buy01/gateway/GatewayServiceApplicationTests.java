a78c8a7ecaef10f5fe6e4b002b3510e6f9168963
Husain Jaafar <husain.local@example.com>
2026-07-25 02:29:41 +0300

    test(gateway): launch reactive context on a random port

--- gateway-service/src/test/java/com/buy01/gateway/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/buy01/gateway/GatewayServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = {
                 "eureka.client.enabled=false",
                 "spring.cloud.discovery.enabled=false"
